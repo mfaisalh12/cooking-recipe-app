0b6b827a50fbf28daa1327ea6b19dfe04ba4316f
seculabBodonng <seculab2022@gmail.com>
2022-12-29 09:14:52 +0700

    book bingung gw bang

--- src/pages/Bookmark.js
+++ src/pages/Bookmark.js
@@ -12,32 +12,37 @@ import Iconicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Destinasi = ({navigation}) => {
+const Desp = ({navigation}) => {
   const [dataTrending, setdataTrending] = useState([
     {
-      namawisata: 'Gorengan',
+      namawisata: 'Gorengan campur',
       image:
         'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      resep :"Goregan",
     },
     {
-      namawisata: 'Roti',
+      namawisata: 'Roti lapis',
       image:
         'https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80',
+      resep :"roti",
     },
     {
-      namawisata: 'Salat',
+      namawisata: 'Salat sayur',
       image:
         'https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      resep :"Salat",
     },
     {
       namawisata: 'Salat + Sayur',
       image:
         'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      resep :"vegaterian",
     },
     {
-      namawisata: 'kue madu',
+      namawisata: 'Kue madu',
       image:
         'https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      resep :"Kue",
     },
   ]);
 
@@ -96,6 +101,13 @@ const Destinasi = ({navigation}) => {
                 }}>
                 {item.namawisata}
               </Text>
+              <Text
+                style={{
+                  color: '#212121',
+                  fontsize: 18,
+                }}>
+                {item.resep}
+              </Text>
             </TouchableOpacity>
           )}
         />
@@ -104,7 +116,7 @@ const Destinasi = ({navigation}) => {
   );
 };
 
-export default Destinasi;
+export default Desp ;
 
 
 const styles = StyleSheet.create({});
